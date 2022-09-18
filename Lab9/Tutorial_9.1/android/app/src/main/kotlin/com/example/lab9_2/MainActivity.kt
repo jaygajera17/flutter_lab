f1592ec5dd61378f0d906eb0d81be489e8d87e54
jaygajera17 <20ceuos042@ddu.ac.in>
2022-09-18 12:35:46 +0530

    my commit

--- Lab9/Tutorial_9.1/android/app/src/main/kotlin/com/example/lab9_2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lab9_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Lab9/Tutorial_9.1/readme.md
+++
@@ -1,1 +0,0 @@
-![image](https://user-images.githubusercontent.com/81226571/189968527-dea87960-d91d-4d67-89a2-e84b96f11e94.png)
